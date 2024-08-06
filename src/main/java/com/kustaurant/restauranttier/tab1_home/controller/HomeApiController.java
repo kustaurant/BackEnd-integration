b728b15da041486ac8b36d93c0c31f85db1ea4e2
imjaehyeong <dlawogud517@gmail.com>
2024-08-06 17:03:35 +0900

    fix: 홈 swagger 오타 수정

--- src/main/java/com/kustaurant/restauranttier/tab1_home/controller/HomeApiController.java
+++ src/main/java/com/kustaurant/restauranttier/tab1_home/controller/HomeApiController.java
@@ -23,7 +23,7 @@ import java.util.stream.Collectors;
 public class HomeApiController {
     private final RestaurantApiService restaurantApiService;
 
-    @Operation(summary = "홈화면 top맛집, 나를 위한 맛집, 슬라이드 이미지 불러오기", description = "top 맛집과 나를 위한 맛집 리스트인 topRestaurantsByRating, restaurantsForMe 을 반환하고 홈의 배너 이미지 url리스트를 반환합니다. 현재 배너 이미지 url은 임시 이미지입니다")
+    @Operation(summary = "홈화면 top맛집, 나를 위한 맛집, 배너 이미지 불러오기", description = "top 맛집과 나를 위한 맛집 리스트인 topRestaurantsByRating, restaurantsForMe 을 반환하고 홈의 배너 이미지 url리스트를 반환합니다. 현재 배너 이미지 url은 임시 이미지입니다")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "restaurant found", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = RestaurantListsResponse.class))}),
             @ApiResponse(responseCode = "404", description = "restaurant not found", content = {@Content(mediaType = "application/json")})
