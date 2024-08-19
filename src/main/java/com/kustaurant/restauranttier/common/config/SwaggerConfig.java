d0c42c9b5a5f2712e8310935b20b94cae9ac44bb
wcwdfu <skb9731@gmail.com>
2024-08-19 09:40:52 +0900

    스웨거 헤더 설정 추가

--- src/main/java/com/kustaurant/restauranttier/common/config/SwaggerConfig.java
+++ src/main/java/com/kustaurant/restauranttier/common/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.kustaurant.restauranttier.common.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,7 +11,9 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
     private static final String API_NAME = "Kustaurant Mobile Application API";
     private static final String API_VERSION = "v1.2.3";
+    private static final String SECURITY_SCHEME_NAME = "Authorization";
     private static final String API_DESCRIPTION = """
+    
     쿠스토랑 모바일 앱 API 문서입니다.
 
     **Version 1.0.1 (2024-07-20)**
@@ -101,6 +105,14 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title(API_NAME)
                         .description(API_DESCRIPTION)
-                        .version(API_VERSION));
+                        .version(API_VERSION))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
+                                .name(SECURITY_SCHEME_NAME)
+                                .type(SecurityScheme.Type.APIKEY)
+                                .in(SecurityScheme.In.HEADER)
+                                .bearerFormat("JWT")
+                                .scheme("bearer")));
     }
 }
