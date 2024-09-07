90c03ca7e79ce5cc6ce645f287b440246380bd03
DingWoonee <slm0523@naver.com>
2024-09-08 02:08:59 +0900

    fix: 티어표와 지도, 식당 상세 정보 api에 대해 auth 버전 추가

--- src/main/java/com/kustaurant/restauranttier/tab3_tier/controller/RestaurantApiController.java
+++ src/main/java/com/kustaurant/restauranttier/tab3_tier/controller/RestaurantApiController.java
@@ -104,6 +104,49 @@ public class RestaurantApiController {
         return new ResponseEntity<>(responseData, HttpStatus.OK);
     }
 
+    @Operation(summary = "식당 상세 화면 정보 불러오기 (Auth 버전)", description = "식당 하나에 대한 상세 정보가 반환됩니다. (mainTier가 -1인 것은 티어가 아직 매겨지지 않은 식당입니다.)\n\n" +
+            "- 반환 값 보충 설명\n\n" +
+            "   - restaurantId: not null\n\n" +
+            "   - restaurantImgUrl: not null\n\n" +
+            "   - mainTier: not null\n\n" +
+            "   - restaurantCuisine: not null\n\n" +
+            "   - restaurantCuisineImgUrl: not null\n\n" +
+            "   - restaurantPosition: not null\n\n" +
+            "   - restaurantName: not null\n\n" +
+            "   - restaurantAddress: not null\n\n" +
+            "   - isOpen: not null\n\n" +
+            "   - businessHours: not null\n\n" +
+            "   - naverMapUrl: not null\n\n" +
+            "   - situationList: **null이거나 빈 배열일 수 있습니다.**\n\n" +
+            "   - partnershipInfo: **null일 수 있습니다.**\n\n" +
+            "   - evaluationCount: not null\n\n" +
+            "   - restaurantScore: **null일 수 있습니다.**(데이터가 없을 경우)\n\n" +
+            "   - isEvaluated: not null\n\n" +
+            "   - isFavorite: not null\n\n" +
+            "   - favoriteCount: not null\n\n" +
+            "   - restaurantMenuList: **null이거나 빈 배열**이 넘어갈 수 있습니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "식당 존재함. 응답 정상 반환.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = RestaurantDetailDTO.class))}),
+            @ApiResponse(responseCode = "404", description = "해당 id를 가진 식당이 없음. 또는 폐업함.", content = {@Content(mediaType = "application/json")})
+    })
+    @GetMapping("/auth/restaurants/{restaurantId}")
+    public ResponseEntity<RestaurantDetailDTO> getRestaurantDetailWithAuth(
+            @PathVariable @Parameter(required = true, description = "식당 id", example = "1") Integer restaurantId,
+            @Parameter(hidden = true) @RequestHeader(value = HttpHeaders.USER_AGENT, required = false) String userAgent,
+            @Parameter(hidden = true) @JwtToken Integer userId
+    ) {
+        // 식당 가져오기
+        Restaurant restaurant = restaurantApiService.findRestaurantById(restaurantId);
+        // 유져 가져오기
+        User user = userService.findUserById(userId);
+        // 식당 데이터 DTO로 변환하기
+        RestaurantDetailDTO responseData = RestaurantDetailDTO.convertRestaurantToDetailDTO(
+                restaurant, restaurantApiService.isEvaluated(restaurant, user), restaurantApiService.isFavorite(restaurant, user), RestaurantConstants.isIOS(userAgent));
+
+        return new ResponseEntity<>(responseData, HttpStatus.OK);
+    }
+
+
     // 즐겨찾기
     @PostMapping("/auth/restaurants/{restaurantId}/favorite-toggle")
     @Operation(summary = "즐겨찾기 추가/해제 토글", description = "즐겨찾기 버튼을 누른 후의 즐겨찾기 상태를 반환합니다.\n\n눌러서 즐겨찾기가 해제된 경우 -> false반환\n\n" +
