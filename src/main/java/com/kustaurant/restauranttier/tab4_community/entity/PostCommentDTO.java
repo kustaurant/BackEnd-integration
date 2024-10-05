38d341916397a74f7c8836aa3cf34cb58b95cf14
imjaehyeong <dlawogud517@gmail.com>
2024-10-05 22:16:12 +0900

    feat: 모바일  커뮤니티 게시글 상세화면 api 에서 댓글 dto에 생성날짜 및 대댓글 리스트 추가

--- src/main/java/com/kustaurant/restauranttier/tab4_community/entity/PostCommentDTO.java
+++ src/main/java/com/kustaurant/restauranttier/tab4_community/entity/PostCommentDTO.java
@@ -1,27 +1,46 @@
 package com.kustaurant.restauranttier.tab4_community.entity;
 
 import com.kustaurant.restauranttier.tab4_community.dto.UserDTO;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
 public class PostCommentDTO {
+    @Schema(description = "댓글 id", example = "100")
     Integer commentId;
+    @Schema(description = "댓글 내용", example = "안녕하세요~")
     private String commentBody;
+    @Schema(description = "댓글 작성자", example = "임재")
     private UserDTO user;
+    @Schema(description = "좋아요 수", example = "3")
     private Integer likeCount;
+    @Schema(description = "싫어요 수", example = "2")
     private Integer dislikeCount;
+    @Schema(description = "댓글이 작성된 날짜", example = "2024-05-19T18:09:06")
+    private LocalDateTime createdAt;
+    @Schema(description = "댓글이 업데이트된 날짜", example = "2024-05-19T18:09:06")
+    private LocalDateTime updatedAt;
+    @Schema(description = "댓글에 달린 대댓글 리스트")
+    private List<PostCommentDTO> repliesList;
 
-    public PostCommentDTO(Integer commentId,String commentBody, UserDTO user, Integer likeCount, Integer dislikeCount) {
+    public PostCommentDTO(Integer commentId,String commentBody, UserDTO user, Integer likeCount, Integer dislikeCount, LocalDateTime createdAt, LocalDateTime updatedAt, List<PostCommentDTO> repliesList) {
         this.commentId= commentId;
         this.commentBody = commentBody;
         this.user = user;
         this.likeCount = likeCount;
         this.dislikeCount = dislikeCount;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.repliesList = repliesList;
     }
 
     public static PostCommentDTO fromEntity(PostComment comment) {
         UserDTO userDTO = UserDTO.fromEntity(comment.user);
-        return new PostCommentDTO(comment.commentId,comment.getCommentBody(), userDTO, comment.getLikeCount(), comment.getDislikeUserList().size());
+        return new PostCommentDTO(comment.commentId,comment.getCommentBody(), userDTO, comment.getLikeCount(), comment.getDislikeUserList().size(),comment.createdAt, comment.updatedAt, comment.getRepliesList().stream().map(PostCommentDTO::fromEntity).toList());
     }}
