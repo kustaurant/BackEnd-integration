52bd75b39e75c53ac12425ebbefb772c12533f79
imjaehyeong <dlawogud517@gmail.com>
2024-12-02 01:24:27 +0900

    fix(postcommentdto): 댓글 좋아요 수 버그 수정
    댓글 좋아요가 토탈 좋아요로 설정되어 반환되던 문제 해결

--- src/main/java/com/kustaurant/restauranttier/tab4_community/dto/PostCommentDTO.java
+++ src/main/java/com/kustaurant/restauranttier/tab4_community/dto/PostCommentDTO.java
@@ -53,7 +53,7 @@ public class PostCommentDTO {
     }
 
     public static PostCommentDTO fromEntity(PostComment comment) {
-        return new PostCommentDTO(comment.getCommentId(), comment.getCommentBody(),comment.getStatus(),comment.getUser(), comment.getLikeCount(), comment.getDislikeUserList().size(), comment.calculateTimeAgo(), comment.getCreatedAt(), comment.getUpdatedAt(), comment.getRepliesList().stream().filter(reply -> reply.getStatus().equals("ACTIVE")).sorted(Comparator.comparing(PostComment::getCreatedAt).reversed()).map(PostCommentDTO::fromEntity).toList());
+        return new PostCommentDTO(comment.getCommentId(), comment.getCommentBody(),comment.getStatus(),comment.getUser(), comment.getLikeUserList().size(), comment.getDislikeUserList().size(), comment.calculateTimeAgo(), comment.getCreatedAt(), comment.getUpdatedAt(), comment.getRepliesList().stream().filter(reply -> reply.getStatus().equals("ACTIVE")).sorted(Comparator.comparing(PostComment::getCreatedAt).reversed()).map(PostCommentDTO::fromEntity).toList());
     }
 
 }
