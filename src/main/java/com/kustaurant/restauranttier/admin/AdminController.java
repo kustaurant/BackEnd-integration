228b48e984ccf443a2b4fc1dc9a777447615f649
Dingwoonee <slm0523@naver.com>
2025-02-28 17:41:27 +0900

    refactor(admin): try-catch문을 깔끔하게 수정함.

--- src/main/java/com/kustaurant/restauranttier/admin/AdminController.java
+++ src/main/java/com/kustaurant/restauranttier/admin/AdminController.java
@@ -23,7 +23,7 @@ public class AdminController {
 
     private final AdminService adminService;
 
-    // 관리자 화면 - 메인
+    // 관리자 화면 - 메인 화면 로드
     @GetMapping
     @PreAuthorize("isAuthenticated() and hasRole('ADMIN')")
     public String admin(Model model) {
@@ -32,7 +32,7 @@ public class AdminController {
         return "admin/admin";
     }
 
-    // 관리자 화면 - 식당 수정 화면
+    // 관리자 화면 - 식당 수정 화면 로드
     @GetMapping("/restaurant/{id}/revise")
     @PreAuthorize("isAuthenticated() and hasRole('ADMIN')")
     public String reviseRestaurant(
@@ -40,32 +40,29 @@ public class AdminController {
             Model model,
             RedirectAttributes redirectAttributes
     ) {
-        // 식당 데이터 가져오기
-        Optional<RestaurantInfoDto> restaurantInfoDtoOptional = adminService.getRestaurantInfo(id);
-        // 식당 데이터가 없을 경우
-        if (restaurantInfoDtoOptional.isEmpty()) {
+        try {
+            model.addAllAttributes(Map.of(
+                    "title", "식당 정보 수정",
+                    "content", "revise-restaurant",
+                    "locations", Arrays.stream(LocationEnum.values())
+                            .map(LocationEnum::getValue)
+                            .filter(l -> !l.equals(LocationEnum.ALL.getValue()))
+                            .toList(),
+                    "cuisines", Arrays.stream(CuisineEnum.values())
+                            .map(CuisineEnum::getValue)
+                            .filter(c -> !c.equals(CuisineEnum.ALL.getValue()) && !c.equals(CuisineEnum.JH.getValue()))
+                            .toList(),
+                    "restaurantInfo", adminService.getRestaurantInfo(id).get(),
+                    "id", id
+            ));
+        } catch (Exception e) {
+            log.error("[AdminController][reviseRestaurant] {}", e.getMessage(), e);
             redirectAttributes.addFlashAttribute("errorMessage", "식당 데이터 조회 또는 변환 과정에서 오류가 발생했습니다.");
-            return "redirect:/admin";
         }
-        // 식당 데이터가 있을 경우 model 데이터 채우기
-        model.addAllAttributes(Map.of(
-                "title", "식당 정보 수정",
-                "content", "revise-restaurant",
-                "locations", Arrays.stream(LocationEnum.values())
-                        .map(LocationEnum::getValue)
-                        .filter(l -> !l.equals(LocationEnum.ALL.getValue()))
-                        .toList(),
-                "cuisines", Arrays.stream(CuisineEnum.values())
-                        .map(CuisineEnum::getValue)
-                        .filter(c -> !c.equals(CuisineEnum.ALL.getValue()) && !c.equals(CuisineEnum.JH.getValue()))
-                        .toList(),
-                "restaurantInfo", restaurantInfoDtoOptional.get(),
-                "id", id
-        ));
         return "admin/admin";
     }
 
-    // 식당 수정 요청
+    // 식당 정보 수정 요청
     @PostMapping("/restaurant/{id}/revise")
     @PreAuthorize("isAuthenticated() and hasRole('ADMIN')")
     public String reviseRestaurant(
@@ -76,18 +73,15 @@ public class AdminController {
         // 데이터 갱신
         try {
             adminService.updateRestaurantInfo(id, restaurantInfoDto);
+            redirectAttributes.addFlashAttribute("successMessage", "식당 데이터 수정이 성공적으로 완료되었습니다.");
         } catch (Exception e) { // 트랜잭션 과정에서 예외 발생 시 오류 페이지
-            log.error("[AdminController][reviseRestaurantRequest] {}", e.getMessage(), e);
+            log.error("[AdminController][reviseRestaurant] {}", e.getMessage(), e);
             redirectAttributes.addFlashAttribute("errorMessage", "식당 정보 갱신 과정에서 오류가 발생했습니다.");
-            return "redirect:/admin";
         }
-
-        // 트랜잭션 정상 처리 시, 데이터 채우고 관리자 메인 화면으로
-        redirectAttributes.addFlashAttribute("successMessage", "식당 데이터 수정이 성공적으로 완료되었습니다.");
         return "redirect:/admin";
     }
 
-    // 관리자 화면 - 식당 추가 화면
+    // 관리자 화면 - 식당 추가 화면 로드
     @GetMapping("/restaurant/add")
     @PreAuthorize("isAuthenticated() and hasRole('ADMIN')")
     public String addRestaurant(Model model) {
@@ -107,7 +101,7 @@ public class AdminController {
         return "admin/admin";
     }
 
-    // 식당 추가 요청
+    // 식당 정보 추가 요청
     @PostMapping("/restaurant/add")
     @PreAuthorize("isAuthenticated() and hasRole('ADMIN')")
     public String addRestaurant(
@@ -116,13 +110,11 @@ public class AdminController {
     ) {
         try {
             adminService.addRestaurantInfos(dataMap);
+            redirectAttributes.addFlashAttribute("successMessage", "식당 데이터 추가가 성공적으로 완료되었습니다.");
         } catch (Exception e) { // 트랜잭션 과정에서 예외 발생 시 오류 페이지
             log.error("[AdminController][addRestaurant] {}", e.getMessage(), e);
             redirectAttributes.addFlashAttribute("errorMessage", "식당 데이터 저장 과정에서 오류가 발생했습니다.");
-            return "redirect:/admin";
         }
-        // 트랜잭션 정상 처리 시, 데이터 채우고 관리자 메인 화면으로
-        redirectAttributes.addFlashAttribute("successMessage", "식당 데이터 추가가 성공적으로 완료되었습니다.");
         return "redirect:/admin";
     }
 }
