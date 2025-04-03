be7291f7c15c91204e412865101990849a2ce9d7
wcwdfu <skb9731@gmail.com>
2025-04-03 22:26:20 +0900

    사용하는 모달ID를 상수로 명명한다

--- src/main/java/com/kustaurant/kustaurant/common/modal/service/HomeModalService.java
+++ src/main/java/com/kustaurant/kustaurant/common/modal/service/HomeModalService.java
@@ -11,9 +11,10 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class HomeModalService {
     private final HomeModalRepository homeModalRepository;
+    private static final int MODAL_ID_IS_ONLY_ONE = 1;
 
     public HomeModalEntity get() {
-        return homeModalRepository.findById(1)
+        return homeModalRepository.findById(MODAL_ID_IS_ONLY_ONE)
                 .filter(modal -> modal.getExpiredAt().isAfter(LocalDateTime.now()))
                 .orElse(null);
     }
