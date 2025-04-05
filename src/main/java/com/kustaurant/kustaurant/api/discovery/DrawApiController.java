2a30721ed50bfb9d775b2b1a82d51bb11bb2171b
Dingwoonee <slm0523@naver.com>
2025-04-05 15:21:16 +0900

    refactor(refactor/tier): draw service 리팩토링

--- src/main/java/com/kustaurant/kustaurant/api/discovery/DrawApiController.java
+++ src/main/java/com/kustaurant/kustaurant/api/discovery/DrawApiController.java
@@ -1,5 +1,6 @@
 package com.kustaurant.kustaurant.api.discovery;
 
+import com.kustaurant.kustaurant.common.discovery.service.DiscoveryDrawService;
 import com.kustaurant.kustaurant.common.restaurant.infrastructure.restaurant.RestaurantEntity;
 import com.kustaurant.kustaurant.common.restaurant.service.RestaurantApiService;
 import com.kustaurant.kustaurant.global.exception.ErrorResponse;
@@ -26,8 +27,8 @@ import java.util.*;
 @RequiredArgsConstructor
 public class DrawApiController {
 
-    private final RestaurantApiService restaurantApiService;
-    private final DrawApiSerivce drawApiSerivce;
+    private final DiscoveryDrawService drawService;
+
     @Operation(summary = "뽑기 버튼 클릭 시 조건에 맞는 식당 리스트 반환", description = "위치와 음식 종류에 맞는 식당 중 랜덤으로 30개를 추출하여 반환합니다. 이 30개 중에서 하나를 뽑는 방식으로 뽑기가 진행되고 뽑는 방식은 클라이언트에서 진행됩니다. 조건이 맞는 식당이 30개가 안될 경우 조건에 맞는 식당을 중복으로 추가하여 30개 수량을 맞추고 랜덤으로 섞어서 반환합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "해당 조건에 맞는 맛집이 성공적으로 반환되었습니다.", content = {@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = RestaurantTierDTO.class)))}),
@@ -40,19 +41,7 @@ public class DrawApiController {
             @Parameter(schema = @Schema(type = "string"), example = "L1,L2,L3 또는 ALL", description = "위치입니다. ALL(전체)을 제외하고 복수 선택 가능(콤마로 구분). ALL이 포함되어 있으면 나머지 카테고리는 무시합니다. (ALL:전체, L1:건입~중문, L2:중문~어대, L3:후문, L4:정문, L5:구의역)")
             @LocationList List<String> locations
     ) {
-
-        List<RestaurantEntity> restaurantList = restaurantApiService.getRestaurantsByCuisinesAndSituationsAndLocations(cuisines, null, locations,null,true);
-        // 조건에 맞는 식당이 없을 경우 404 에러 반환
-        if (restaurantList.isEmpty()) {
-            throw new OptionalNotExistException("해당 조건에 맞는 맛집이 존재하지 않습니다.");
-        }
-        // 랜덤으로 30개의 식당 선택
-        List<RestaurantEntity> randomRestaurantList = drawApiSerivce.getRandomSubList(restaurantList, 30);
-        // DTO로 변환
-        List<RestaurantTierDTO> drawRestaurantListDTOs = randomRestaurantList.stream()
-                .map(restaurant -> RestaurantTierDTO.convertRestaurantToTierDTO(restaurant, null, null, null))
-                .toList();
-        return new ResponseEntity<>(drawRestaurantListDTOs, HttpStatus.OK);
+        return new ResponseEntity<>(drawService.draw(cuisines, locations), HttpStatus.OK);
     }
 
 }
