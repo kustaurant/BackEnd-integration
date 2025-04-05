f0819caf645d9947fd2c5905abc3f0bfc9cdf618
Dingwoonee <slm0523@naver.com>
2025-04-05 16:08:42 +0900

    fix(refactor/tier): 컴파일 에러 간단한거 수정

--- src/main/java/com/kustaurant/kustaurant/common/discovery/service/DiscoverySearchService.java
+++ src/main/java/com/kustaurant/kustaurant/common/discovery/service/DiscoverySearchService.java
@@ -2,7 +2,7 @@ package com.kustaurant.kustaurant.common.discovery.service;
 
 import com.kustaurant.kustaurant.common.discovery.domain.RestaurantTierDTO;
 import com.kustaurant.kustaurant.common.discovery.service.port.DiscoveryRepository;
-import com.kustaurant.kustaurant.common.restaurant.domain.Restaurant;
+import jakarta.annotation.Nullable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,12 +15,14 @@ public class DiscoverySearchService {
     private final DiscoveryRepository discoveryRepository;
     private final DiscoveryAssembler discoveryAssembler;
 
-    public List<RestaurantTierDTO> search(String[] kwList, Integer userId) {
+    public List<RestaurantTierDTO> search(String[] kwList, @Nullable Integer userId) {
         // 검색 결과 가져와서 DTO로 매핑
         List<RestaurantTierDTO> dtoList = discoveryRepository.search(kwList)
                 .stream().map(DiscoveryMapper::toDto).toList();
 
         // 각 식당의 즐찾여부, 평가여부, 랭킹 정보 채우기
         discoveryAssembler.enrichDtoList(userId, dtoList, null);
+
+        return dtoList;
     }
 }
