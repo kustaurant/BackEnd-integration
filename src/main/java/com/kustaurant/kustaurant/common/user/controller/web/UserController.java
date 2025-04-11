6323dae5caedc6a77c1147827a64d26988872a0e
wcwdfu <skb9731@gmail.com>
2025-04-11 21:51:19 +0900

    [refactor] 안쓰는클래스, 코드제거

--- src/main/java/com/kustaurant/kustaurant/common/user/controller/web/UserController.java
+++ src/main/java/com/kustaurant/kustaurant/common/user/controller/web/UserController.java
@@ -1,19 +1,10 @@
 package com.kustaurant.kustaurant.common.user.controller.web;
 
-import com.kustaurant.kustaurant.global.webUser.CustomOAuth2UserService;
-import com.kustaurant.kustaurant.global.webUser.UserCreateForm;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
-import jakarta.validation.Valid;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
@@ -26,9 +17,6 @@ import java.util.Random;
 @RequestMapping("/user")
 public class UserController {
 
-    private final HttpSession httpSesseion;
-    private final CustomOAuth2UserService customOAuth2UserService;
-    private final Logger log = LoggerFactory.getLogger(getClass());
     private final List<String> loginImgUrlList = List.of(
             "/img/login/food1.jpg",
             "/img/login/food1.jpg",
@@ -82,43 +70,5 @@ public class UserController {
     public ResponseEntity<String> isLogin () {
         return ResponseEntity.ok("true");
     }
-
-
-    // signup에 Get => 회원가입 템플릿
-    @GetMapping("/signup")
-    public String signup(UserCreateForm userCreateForm) {
-        return "signup_form";
-    }
-    // signup에 POST => 회원가입 진행 -> db에 user 생성
-    @PostMapping("/signup")
-    public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return "signup_form";
-        }
-        // 비밀번호와 비밀번호 확인에 입력 값이 다를때 예외처리
-        if (!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())) {
-            bindingResult.rejectValue("password2", "passwordInCorrect",
-                    "2개의 패스워드가 일치하지 않습니다.");
-            return "signup_form";
-        }
-        
-        // 닉네임과 email이 겹치는 회원가입일때 예외처리
-        try {
-            
-            // 일단 service 안에 throw DataIntegrityViolationException 처리 해놨음. 엔티티 수정 필요
-            //userService.create(userCreateForm.getUserId(),
-            //        userCreateForm.getEmail(), userCreateForm.getPassword1(), userCreateForm.getNickname());
-        } catch (DataIntegrityViolationException e) {
-            e.printStackTrace();
-            bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
-            return "signup_form";
-        } catch (Exception e) {
-            e.printStackTrace();
-            bindingResult.reject("signupFailed", e.getMessage());
-            return "signup_form";
-        }
-        return "redirect:/user/login";
-    }
-
 
 }
