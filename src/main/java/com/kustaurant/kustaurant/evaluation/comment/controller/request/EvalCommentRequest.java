e9ec1b8aa945931ea3c4f67e8a1e93b146f44da4
Dingwoonee <slm0523@naver.com>
2025-07-11 00:45:10 +0900

    chore: 자잘한 수정

--- src/main/java/com/kustaurant/kustaurant/evaluation/comment/controller/request/EvalCommentRequest.java
+++ src/main/java/com/kustaurant/kustaurant/evaluation/comment/controller/request/EvalCommentRequest.java
@@ -7,7 +7,7 @@ import jakarta.validation.constraints.Size;
 public record EvalCommentRequest(
         @Schema(description = "댓글 본문", example = "저두 이 의견에 동의합니당")
         @NotBlank(message = "댓글 내용은 비어 있을 수 없습니다.")
-        @Size(min = 10, max = 1000, message = "댓글은 10자 이상 1000자 이하여야 합니다.")
+        @Size(max = 1000, message = "댓글은 1000자 이하여야 합니다.")
         String body
 ) {
 }
