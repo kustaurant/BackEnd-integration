398be4aae3f608231b4fe6405570ad8b80e6af1e
wcwdfu <skb9731@gmail.com>
2025-09-25 14:26:57 +0900

    중형 테스트 정상 작동 해결

--- server/kustaurant/src/main/java/com/kustaurant/kustaurant/KustaurantApplication.java
+++ server/kustaurant/src/main/java/com/kustaurant/kustaurant/KustaurantApplication.java
@@ -4,8 +4,10 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @OpenAPIDefinition(servers = {
@@ -14,6 +16,14 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableCaching
 @SpringBootApplication(scanBasePackages = {
+        "com.kustaurant.kustaurant",
+        "com.kustaurant.jpa"
+})
+@EnableJpaRepositories(basePackages = {
+        "com.kustaurant.kustaurant",
+        "com.kustaurant.jpa"
+})
+@EntityScan(basePackages = {
         "com.kustaurant.kustaurant",
         "com.kustaurant.jpa"
 })
