2cbd207b24bdfc73aed30b89929519f554d8aa31
Dingwoonee <slm0523@naver.com>
2025-09-26 08:29:12 +0900

    fix: rating initializer prod 추가

--- server/kustaurant/src/main/java/com/kustaurant/kustaurant/rating/service/RatingInitializer.java
+++ server/kustaurant/src/main/java/com/kustaurant/kustaurant/rating/service/RatingInitializer.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
-@Profile("local")
+@Profile({"local", "prod"})
 @Component
 @RequiredArgsConstructor
 public class RatingInitializer {
