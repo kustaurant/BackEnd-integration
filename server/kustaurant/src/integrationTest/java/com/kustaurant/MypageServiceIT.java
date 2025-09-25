d3180597bf9f838b2ed1ed64e37f294e3742273f
Dingwoonee <slm0523@naver.com>
2025-09-26 02:49:44 +0900

    테스트 제외시킴

--- server/kustaurant/src/integrationTest/java/com/kustaurant/MypageServiceIT.java
+++ server/kustaurant/src/integrationTest/java/com/kustaurant/MypageServiceIT.java
@@ -26,7 +26,6 @@ import com.kustaurant.kustaurant.user.user.domain.Nickname;
 import com.kustaurant.kustaurant.user.user.service.port.UserRepository;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.redis.core.StringRedisTemplate;
