c6c52b2e297c4ba5da4aac88a846e0b03c164d14
wcwdfu <skb9731@gmail.com>
2025-09-28 18:53:24 +0900

    fix(web): home 건대 top맛집 확인 깨짐 이슈 해결

--- server/kustaurant/src/main/java/com/kustaurant/kustaurant/admin/notice/NoticeService.java
+++ server/kustaurant/src/main/java/com/kustaurant/kustaurant/admin/notice/NoticeService.java
@@ -1,8 +1,5 @@
-package com.kustaurant.kustaurant.admin.notice.service;
+package com.kustaurant.kustaurant.admin.notice;
 
-import com.kustaurant.kustaurant.admin.notice.domain.Notice;
-import com.kustaurant.kustaurant.admin.notice.infrastructure.NoticeEntity;
-import com.kustaurant.kustaurant.admin.notice.infrastructure.NoticeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
