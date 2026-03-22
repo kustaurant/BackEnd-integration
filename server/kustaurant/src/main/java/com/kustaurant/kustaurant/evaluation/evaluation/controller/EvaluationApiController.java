569fcbaf438ac1c9c41627b19c72fb5bc614bc35
Dingwoonee <slm0523@naver.com>
2026-03-22 20:41:30 +0900

    fix: evaluationDto에 situationIds가 null로 들어오는 것 수정

--- server/kustaurant/src/main/java/com/kustaurant/kustaurant/evaluation/evaluation/controller/EvaluationApiController.java
+++ server/kustaurant/src/main/java/com/kustaurant/kustaurant/evaluation/evaluation/controller/EvaluationApiController.java
@@ -12,6 +12,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -43,6 +45,9 @@ public class EvaluationApiController implements EvaluationApiDoc {
             throw new ParamException("평가 점수가 필요합니다.");
         }
 
+        if (evaluationDTO.getEvaluationSituations() == null) {
+            evaluationDTO.setEvaluationSituations(new ArrayList<>());
+        }
         log.info("[evaluation] user id: {}, data: {}", user.id(), evaluationDTO);
 
         // 평가 데이터 저장 또는 업데이트
