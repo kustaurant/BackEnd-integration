dac017244a1a4c9b8b18e583d18635cd45779828
Wcwdfu <skb9731@gmail.com>
2026-03-28 19:52:30 +0900

    feat: crawl타겟별 caption클래스 추가

--- common/jpa/src/main/java/com/kustaurant/jpa/restaurant/enums/PartnershipTarget.java
+++ common/jpa/src/main/java/com/kustaurant/jpa/restaurant/enums/PartnershipTarget.java
@@ -1,17 +1,18 @@
 package com.kustaurant.jpa.restaurant.enums;
 
 public enum PartnershipTarget {
-    ALL("재학생모두"),
-    ENGINEERING("공과대학"),
-    EDUCATION("사범대학"),
-    SOCIAL_SCIENCE("사회과학대학"),
-    LIFE_SCIENCE("생명과학대학"),
-    VETERINARY("수의과대학"),
+    ALL("재학생모두"), //
+    ENGINEERING("공과대학"), //식당없음
+    LIBERAL("문과대학"), //식당없음
+    EDUCATION("사범대학"), //식당없음
+    SOCIAL_SCIENCE("사회과학대학"), //
+    LIFE_SCIENCE("생명과학대학"), //
+    VETERINARY("수의과대학"), //
     ART_DESIGN("예술디자인대학"),
-    CONVERGENCE_SCI_TECH("융합과학기술원"),
-    SCIENCE("이과대학"),
-    CLUBUNION("동아리연합"),
-    WELFARE("학생복지위원회");
+    CONVERGENCE_SCI_TECH("융합과학기술원"), //식당없음
+    SCIENCE("이과대학"), //식당없음
+    CLUBUNION("동아리연합"), //식당없음
+    WELFARE("학생복지위원회"); //식당없음
 
 
     private final String koreanName;
