b7533b37c6077daabdd1df1311d7c6dffcef9263
Wcwdfu <skb9731@gmail.com>
2026-04-03 14:10:08 +0900

    크롤러서버 재배포용 커밋

--- server/crawler/src/main/java/com/kustaurant/crawler/IGpartnership/service/Crawler.java
+++ server/crawler/src/main/java/com/kustaurant/crawler/IGpartnership/service/Crawler.java
@@ -17,7 +17,7 @@ import java.util.List;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class Crawler {
+public class Crawler { //
 
     private final IGSessionManager sessionManager;
     private final IGFeedCollector feedCollector;
